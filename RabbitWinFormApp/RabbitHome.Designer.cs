eb166d69d6e23fc2e09e6e47b939954eaecb26a8
yuzhe <thecourageyu@gmail.com>
2018-08-14 11:55:14 +0800

    Add RabbitWinFormApp.

---
+++ RabbitWinFormApp/RabbitHome.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RabbitWinFormApp
+{
+    partial class RabbitHome
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RabbitHome));
+            this.btnCarrot = new System.Windows.Forms.Button();
+            this.labCarrot = new System.Windows.Forms.Label();
+            this.btnBattle = new System.Windows.Forms.Button();
+            this.TestBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnCarrot
+            // 
+            this.btnCarrot.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarrot.Location = new System.Drawing.Point(567, 391);
+            this.btnCarrot.Name = "btnCarrot";
+            this.btnCarrot.Size = new System.Drawing.Size(122, 47);
+            this.btnCarrot.TabIndex = 0;
+            this.btnCarrot.Text = "Collect Carrot";
+            this.btnCarrot.UseVisualStyleBackColor = true;
+            this.btnCarrot.Click += new System.EventHandler(this.btnCarrot_Click);
+            // 
+            // labCarrot
+            // 
+            this.labCarrot.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labCarrot.Location = new System.Drawing.Point(503, 9);
+            this.labCarrot.Name = "labCarrot";
+            this.labCarrot.Size = new System.Drawing.Size(98, 23);
+            this.labCarrot.TabIndex = 1;
+            this.labCarrot.Text = "Carrot";
+            this.labCarrot.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBattle
+            // 
+            this.btnBattle.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBattle.Location = new System.Drawing.Point(422, 391);
+            this.btnBattle.Name = "btnBattle";
+            this.btnBattle.Size = new System.Drawing.Size(122, 47);
+            this.btnBattle.TabIndex = 2;
+            this.btnBattle.Text = "Battle";
+            this.btnBattle.UseVisualStyleBackColor = true;
+            this.btnBattle.Click += new System.EventHandler(this.btnBattle_Click);
+            // 
+            // TestBox
+            // 
+            this.TestBox.Location = new System.Drawing.Point(444, 51);
+            this.TestBox.Name = "TestBox";
+            this.TestBox.Size = new System.Drawing.Size(217, 134);
+            this.TestBox.TabIndex = 3;
+            this.TestBox.Text = "";
+            // 
+            // RabbitHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(721, 450);
+            this.Controls.Add(this.TestBox);
+            this.Controls.Add(this.btnBattle);
+            this.Controls.Add(this.labCarrot);
+            this.Controls.Add(this.btnCarrot);
+            this.Name = "RabbitHome";
+            this.Text = "Rabbit&Rabbit";
+            this.Load += new System.EventHandler(this.RabbitHome_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarrot;
+        private System.Windows.Forms.Label labCarrot;
+        private System.Windows.Forms.Button btnBattle;
+        private System.Windows.Forms.RichTextBox TestBox;
+    }
+}
+
---
+++ RabbitWinFormApp/RabbitHome.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CharactersLib;
+using BattleWinFormApp;
+
+namespace RabbitWinFormApp
+{
+    public partial class RabbitHome : Form
+    {
+
+        DateTime TimeTheFirstCarrot = DateTime.Now;
+        private Battle BattleWindow;
+        int numCarrot = 12;
+        //string PicDir = "E:\\GitHub\\RabbitWinFormApp\\Reference\\RabbitHome";
+        string PicDir = "E:\\GitHub\\RabbitWinFormApp\\References\\RabbitHome";
+
+        /*public class Rabbit : Characters 
+        {
+            private int numCarrot;
+            public int NumCarrot { get; set; }
+     
+            public Rabbit(int nationalId, string name, numCarrot) : base(nationalId, name)
+            {
+                this.numCarrot = 100;
+            }
+            
+            protected override Characters Generate(int nationalId, string name)
+            {
+              
+                return base.Generate(nationalId, name);
+            }
+        }*/
+
+        public RabbitHome()
+        {
+            
+            InitializeComponent();
+            
+            int tmp = 1;
+            while(tmp < 1000)
+            {
+                tmp = tmp + 1;
+                labCarrot.Text = tmp.ToString();
+            }
+            //GenerateRabbit();
+        }
+
+        /*
+        public void GenerateRabbit()
+        {
+            Characters RabbitObj = Characters.Generate(Id, Names[Id - 1]);
+            EnemyLevel.Text = Enemy.Level.ToString();
+            EnemyName.Text = Enemy.Name;
+            EnemyHP.Text = Enemy.Hp.ToString();
+            EnemyMaxHP.Text = Enemy.MaxHp.ToString();
+        }
+        */
+        private void DrawPage(int PicId)
+        {
+            string tmpString;
+            if(PicId == 0)
+            {
+                PicId = 12;
+            }
+            
+            tmpString = PicId <= 9 ? "0" + PicId.ToString() : PicId.ToString();
+            
+            
+            //tmpString = PicDir + "/" + tmpString + "-1-01.png";
+            
+            TestBox.Text = PicDir + "\\" + tmpString + "-1-01.png";
+            
+            /*
+            this.BackgroundImage = new Bitmap(PicDir + "/" + tmpString + "-1-01.png");
+            Thread.Sleep(1000);
+            this.BackgroundImage = new Bitmap(PicDir + "/" + tmpString + "-2-01.png");
+            Thread.Sleep(1000);
+            this.BackgroundImage = new Bitmap(PicDir + "/" + tmpString + "-3-01.png");
+            Thread.Sleep(1000); 
+            */
+            this.BackgroundImage = new Bitmap(PicDir + "/" + tmpString + "-4-01.png");
+           
+        }
+
+        private void btnCarrot_Click(object sender, EventArgs e)
+        {
+
+            numCarrot = numCarrot - 1;
+            if(numCarrot >= 1)
+            {
+                if (numCarrot == 12 - 1)
+                {
+                    TimeTheFirstCarrot = DateTime.Now;
+                }
+                DrawPage(12 - numCarrot);
+            }
+            else
+            {
+                DateTime TimeForNoCarrot = DateTime.Now;
+                
+                TimeSpan span = TimeForNoCarrot.Subtract(TimeTheFirstCarrot);
+                TestBox.Text = span.ToString() + "s";
+                while (span.Seconds <= 6)
+                {
+
+                    Thread.Sleep(1000*10);
+                    
+                    DateTime TimeNow = DateTime.Now;
+                    span = TimeNow.Subtract(TimeTheFirstCarrot);
+                    TestBox.Text = span.ToString() + "s";
+                }
+                numCarrot = 12;
+                DrawPage(0);
+            }
+        }
+
+        private void RabbitHome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBattle_Click(object sender, EventArgs e)
+        {
+            this.BattleWindow = new Battle();
+            //BattleWindow.RabbitHP =
+            BattleWindow.Show();
+        }
+    }
+}
